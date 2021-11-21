75a081d4232b3d7b7761736e4c58677b6a23bffb
jacobbowdoin <jwrunner7@gmail.com>
2021-11-22 07:35:30 +0800

    change to messages

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
   import { Collection, Doc, getDocument, hello } from 'sveltefirets';
 
-  getDocument('templates/CrJauqXJ249p7r0OxE53').then((template) => {
-    console.log({ template }); // will be null on server
+  getDocument('messages/CrJauqXJ249p7r0OxE53').then((message) => {
+    console.log({ message }); // will be null on server
   });
 </script>
 
 <h1>{hello('Dan')}</h1>
 
-<Doc path={`templates/CrJauqXJ249p7r0OxE53`} startWith={{ name: 'Bill' }} log let:data={template}>
-  <pre>{JSON.stringify(template, null, 2)}</pre>
+<Doc path={`messages/CrJauqXJ249p7r0OxE53`} startWith={{ name: 'Bill' }} log let:data={message}>
+  <pre>{JSON.stringify(message, null, 2)}</pre>
 </Doc>
 
-<Collection path="templates" startWith={[{ name: 'Bob' }, { name: 'Betty' }]} let:data={people} log>
-  {#each people as person}
-    <pre>{JSON.stringify(person.name, null, 2)}</pre>
+<Collection path="messages" startWith={[{ name: 'Bob' }, { name: 'Betty' }]} let:data={messages} log>
+  {#each messages as message}
+    <pre>{JSON.stringify(message.name, null, 2)}</pre>
   {/each}
 </Collection>
 
