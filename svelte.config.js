beef9c7da3304e687628617bf80a7682343eb67b
jacobbowdoin <jwrunner7@gmail.com>
2021-11-22 07:34:57 +0800

    update for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		adapter: vercel(),
 		target: '#svelte',
 		package: {
 			dir: './package',
