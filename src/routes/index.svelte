3de00ca96382c3840f62df9bc49480255bfa1da1
jacobbowdoin <jwrunner7@gmail.com>
2021-11-22 13:31:47 +0800

    setup simple CRUD demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
-  import { Collection, Doc, getDocument, hello } from 'sveltefirets';
-import Config from './_Config.svelte';
+  import { getDocument, platform } from 'sveltefirets';
+  import Config from './_demo-lib/Config.svelte';
+  import Create from './_demo-lib/Create.svelte';
+  import ReadSingle from './_demo-lib/ReadSingle.svelte';
+  import ReadUpdateDelete from './_demo-lib/ReadUpdateDelete.svelte';
 
-  getDocument('messages/CrJauqXJ249p7r0OxE53').then((message) => {
+  getDocument('messages/7CFRkX2oVTSr6SgwkT9D').then((message) => {
     console.log({ message }); // will be null on server
   });
 </script>
 
-<h1>{hello('Dan')}</h1>
+<h1>SvelteFire TS</h1>
 
-<Doc path={`messages/CrJauqXJ249p7r0OxE53`} startWith={{ name: 'Bill' }} log let:data={message}>
-  <pre>{JSON.stringify(message, null, 2)}</pre>
-</Doc>
-
-<Collection path="messages" startWith={[{ name: 'Bob' }, { name: 'Betty' }]} let:data={messages} log>
-  {#each messages as message}
-    <pre>{JSON.stringify(message.name, null, 2)}</pre>
-  {/each}
-</Collection>
-
-<!-- queryConstraints={[where('createdBy', '==', $user.uid), orderBy('updatedAt', 'desc')]}> -->
+{#if platform === 'client'}
+  <p>
+    <strong>Tip:</strong>
+    Open the browser console for development logging.
+  </p>
+{:else}
+  <p>Came from server</p>
+{/if}
 
 <Config />
+
+<Create />
+
+<ReadUpdateDelete />
+
+<ReadSingle />
