639fd4d012d3e9f2350bc9a65855376668998c8c
jacobbowdoin <jwrunner7@gmail.com>
2021-12-26 07:23:10 +0800

    remove unneeded duplicate code

--- src/lib/server/firestore.ts
+++ src/lib/server/firestore.ts
@@ -1,5 +1,7 @@
 // must pass in firebaseApp because getFirestore() function always returns undefined when trying to fetch default app on its own accord - I think because it's designed to work in the client once Firestore has been initialized and not in one synchronous server-side operation
 
+// not using lite versions on server because on client we favor using full firestore cache-first versions instead of a fetch request on every data request (which count as a read even when a user hovers over a link with sveltekite:prefetch for example) and server and client must be using both lite or both regular firestore otherwise we will have errors when trying to pass in queryConstraints from the opposite package.
+
 import type { FirebaseApp } from 'firebase/app';
 import {
   getFirestore,
