20e4736b76c768d9b80f94e176218e05ee9afea2
jacobbowdoin <jwrunner7@gmail.com>
2022-05-20 20:34:30 +0800

    add a little

--- packages/demo/src/routes/__layout.svelte
+++ packages/demo/src/routes/__layout.svelte
@@ -1,11 +1,10 @@
 <script context="module" lang="ts">
-  // import { initFirebase } from 'sveltefirets';
-  // import { firebaseConfig } from '$lib/firebaseConfig';
+  import { initFirebase } from 'sveltefirets';
+  import { firebaseConfig } from '$lib/firebaseConfig';
   import type { Load } from '@sveltejs/kit';
   export const load: Load = async () => {
-    // const firebaseApp = initFirebase(firebaseConfig);
-    // return { stuff: { firebaseApp } };
-    return { };
+    const firebaseApp = initFirebase(firebaseConfig);
+    return { stuff: { firebaseApp } };
   };
 </script>
 
--- packages/sveltefirets/src/lib/server/init.ts
+++ packages/sveltefirets/src/lib/server/init.ts
@@ -1,16 +1,18 @@
-import { initializeApp, getApps, type FirebaseApp, type FirebaseOptions } from 'firebase/app';
+import type { FirebaseApp, FirebaseOptions } from 'firebase/app';
+// import { initializeApp, getApps, type FirebaseApp, type FirebaseOptions } from 'firebase/app';
 
-let firebaseApp: FirebaseApp = null;
+const firebaseApp: FirebaseApp = null;
 export function initFirebase(config: FirebaseOptions) {
-  if (firebaseApp) {
-    return firebaseApp;
-  }
+  return null;
+  // if (firebaseApp) {
+  //   return firebaseApp;
+  // }
   
-  if (getApps().length) {
-    firebaseApp = getApps()[0];
-  } else {
-    firebaseApp = initializeApp(config);
-    console.log('firebase initialized on server');
-  }
-  return firebaseApp;
+  // if (getApps().length) {
+  //   firebaseApp = getApps()[0];
+  // } else {
+  //   firebaseApp = initializeApp(config);
+  //   console.log('firebase initialized on server');
+  // }
+  // return firebaseApp;
 }
