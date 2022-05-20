f81fde20887e9ff1be3dbbc0965bc208a8f2047e
jacobbowdoin <jwrunner7@gmail.com>
2022-05-20 20:31:10 +0800

    remove init

--- packages/demo/src/routes/__layout.svelte
+++ packages/demo/src/routes/__layout.svelte
@@ -1,10 +1,11 @@
 <script context="module" lang="ts">
-  import { initFirebase } from 'sveltefirets';
-  import { firebaseConfig } from '$lib/firebaseConfig';
+  // import { initFirebase } from 'sveltefirets';
+  // import { firebaseConfig } from '$lib/firebaseConfig';
   import type { Load } from '@sveltejs/kit';
   export const load: Load = async () => {
-    const firebaseApp = initFirebase(firebaseConfig);
-    return { stuff: { firebaseApp } };
+    // const firebaseApp = initFirebase(firebaseConfig);
+    // return { stuff: { firebaseApp } };
+    return { };
   };
 </script>
 
