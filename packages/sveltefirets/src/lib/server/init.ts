0c173d4ad530d966cabea14e20ca06cebca9dbe3
jacobbowdoin <jwrunner7@gmail.com>
2022-06-04 11:39:53 +0800

    turn server init back on

--- packages/sveltefirets/src/lib/server/init.ts
+++ packages/sveltefirets/src/lib/server/init.ts
@@ -1,19 +1,16 @@
-// import type { FirebaseApp, FirebaseOptions } from 'firebase/app';
-// import { initializeApp, getApps, type FirebaseApp, type FirebaseOptions } from 'firebase/app';
+import { initializeApp, getApps, type FirebaseApp, type FirebaseOptions } from 'firebase/app';
 
-// const firebaseApp: FirebaseApp = null;
-// export function initFirebase(config: FirebaseOptions) {
-export function initFirebase(config: any) {
-  return null;
-  // if (firebaseApp) {
-  //   return firebaseApp;
-  // }
-  
-  // if (getApps().length) {
-  //   firebaseApp = getApps()[0];
-  // } else {
-  //   firebaseApp = initializeApp(config);
-  //   console.log('firebase initialized on server');
-  // }
-  // return firebaseApp;
+let firebaseApp: FirebaseApp = null;
+export function initFirebase(config: FirebaseOptions) {
+  if (firebaseApp) {
+    return firebaseApp;
+  }
+
+  if (getApps().length) {
+    firebaseApp = getApps()[0];
+  } else {
+    firebaseApp = initializeApp(config);
+    console.log('firebase initialized on server');
+  }
+  return firebaseApp;
 }
